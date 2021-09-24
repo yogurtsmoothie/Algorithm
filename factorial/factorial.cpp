0b805b6bb71183be497da39263fed839c41788f0
yogurtsmoothie <green7170@naver.com>
2021-09-24 19:23:00 +0900

    순환 알고리즘 간소화

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -5,9 +5,5 @@ int factorial(int n) // n! = n * (n-1)!, n>1, 1! = 1, 0! = 1;
 {
 	if (n == 0) return (1);
 	else if (n == 1) return (1);
-	else //if(n > 1)
-	{
-		int prod = n * factorial(n - 1);
-		return prod;
-	}
+	else return(n * factorial(n - 1));
 }

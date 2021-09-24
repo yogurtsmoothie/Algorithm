4d4450f5e15fd59599a5d74a8733bda26ccc0a29
yogurtsmoothie <green7170@naver.com>
2021-09-24 19:26:39 +0900

    완성

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -7,3 +7,10 @@ int factorial(int n) // n! = n * (n-1)!, n>1, 1! = 1, 0! = 1;
 	else if (n == 1) return (1);
 	else return(n * factorial(n - 1));
 }
+
+void main() {
+	int n;
+	cout << "숫자를 입력하세요 : ";
+	cin >> n;
+	cout << n << "! = " << factorial(n) << endl;
+}
